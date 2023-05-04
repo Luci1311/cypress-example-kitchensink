223e5208ef97922660a9894333ede435cf6984ee
Luci1311 <lucian.ionescu@qubiz.com>
2023-05-04 16:17:53 +0300

    added cookie to remove pop-up

--- cypress/e2e/0 - StudyGroup/fields_lambda.cy.js
+++ cypress/e2e/0 - StudyGroup/fields_lambda.cy.js
@@ -3,41 +3,42 @@
 
 describe('Test for Study group on Lambda Test', () => {
 
-    beforeEach(() => {
-        cy.visit('https://www.lambdatest.com/selenium-playground/')
-        cy.contains('Simple Form Demo').click()
-    });
+  beforeEach(() => {
+    cy.setCookie('exit_popup_dismissed', 'closed');
+    cy.visit('https://www.lambdatest.com/selenium-playground/')
+    cy.contains('Simple Form Demo').click()
+  });
 
-    //Cypress.on('uncaught:exception', () => false)
+  //Cypress.on('uncaught:exception', () => false)
     
-    it('input text and validate it', () => {
-        const newItem = 'Test 1234'
+  it('input text and validate it', () => {
+    const newItem = 'Test 1234'
 
-        cy.get('input[id="user-message"]')
+    cy.get('input[id="user-message"]')
             .type(newItem)
-        cy.get('button[id="showInput"]')
+    cy.get('button[id="showInput"]')
             .click()
-        cy.get('p[id="message"]')
+    cy.get('p[id="message"]')
             .should('have.text', newItem)
-    });
+  });
 
-    it('input two values and validate the sum', () => {
-        const val1 = 3
-        const val2 = 4
-        const sum = val1 + val2
+  it('input two values and validate the sum', () => {
+    const val1 = 3
+    const val2 = 4
+    const sum = val1 + val2
 
-        cy.get('input[id="sum1"]')
+    cy.get('input[id="sum1"]')
             .type(val1)
-        cy.get('input[id="sum2"]')
+    cy.get('input[id="sum2"]')
             .type(val2)
-        cy.get('#gettotal > button')
-        //cy.get('button[class="mt-20 mb-10 bg-black text-white rounded px-15 py-5 hover:bg-lambda-900 focus:outline-none"]').eq(1)
-        //cy.get('button[class="mt-20 mb-10 bg-black text-white rounded px-15 py-5 hover:bg-lambda-900 focus:outline-none"]').last()
-        //cy.contains("Get values")
+    cy.get('#gettotal > button')
+    //cy.get('button[class="mt-20 mb-10 bg-black text-white rounded px-15 py-5 hover:bg-lambda-900 focus:outline-none"]').eq(1)
+    //cy.get('button[class="mt-20 mb-10 bg-black text-white rounded px-15 py-5 hover:bg-lambda-900 focus:outline-none"]').last()
+    //cy.contains("Get values")
             .click()
-        cy.get('p[id="addmessage"]')
+    cy.get('p[id="addmessage"]')
             .should('have.text', sum)
-    });
+  });
 
 
 })
