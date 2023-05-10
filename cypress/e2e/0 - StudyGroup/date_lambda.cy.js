ce71051a2ed7c01a1d4db8121410a6880063810e
Luci1311 <lucian.ionescu@qubiz.com>
2023-05-10 15:07:12 +0300

    minor fixes

--- cypress/e2e/0 - StudyGroup/date_lambda.cy.js
+++ cypress/e2e/0 - StudyGroup/date_lambda.cy.js
@@ -20,17 +20,20 @@ describe('Test for Study group on Lambda Test - Date picker', () => {
 
   it('date range picker ex1', () => {
     const todaysDate = dayjs().format('YYYY-MM-DD')
+    cy.log('**Inputs in the date field the current date**')
     cy.get('input[id="birthday"]')
       .type(todaysDate)
+    cy.log('**this command will clear the field and checks if it is empty**')
     cy.get('input[id="birthday"]')
+      .should('have.value', todaysDate)
       .clear()
       .should('have.value', "")
   });
 
-  it('date range picker ex2', () => {
+  it.only('date range picker ex2', () => {
     const startDate = '2023-05-10'
     const endDate = '2023-05-13'
-    const startDateINT =   Number(startDate.slice(0, 10).split('-').join(''))
+    const startDateINT = Number(startDate.slice(0, 10).split('-').join(''))
     const endDateINT = Number(endDate.slice(0, 10).split('-').join(''))
 
     cy.get('input[placeholder="Start date"]')
@@ -38,6 +41,6 @@ describe('Test for Study group on Lambda Test - Date picker', () => {
     cy.get('input[placeholder="End date"]')
       .type(endDate)
 
-    expect (startDateINT).to.be.lt(endDateINT)  
+    expect(startDateINT).to.be.lt(endDateINT)
   })
 })
