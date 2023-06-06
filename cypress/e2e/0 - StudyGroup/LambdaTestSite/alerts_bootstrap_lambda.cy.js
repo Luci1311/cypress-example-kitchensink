a0df3241978154337e6bd9d7125d0b699e22383f
Luci1311 <lucian.ionescu@qubiz.com>
2023-06-06 16:04:11 +0300

    js and boots alerts

---
+++ cypress/e2e/0 - StudyGroup/LambdaTestSite/alerts_bootstrap_lambda.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+import { slowCypressDown } from 'cypress-slow-down'
+slowCypressDown()
+
+describe('Test for Study group on Lambda Test - Tabel', () => {
+
+  beforeEach(() => {
+    cy.setCookie('exit_popup_dismissed', 'closed');
+    cy.visit('https://www.lambdatest.com/selenium-playground/')
+    cy.contains('Bootstrap Alerts').click()
+  });
+
+  Cypress.on('uncaught:exception', () => false)
+
+
+  it('Autoclosable Success Message', () => {
+    cy.contains('button', 'Autoclosable Success Message')
+      .click()
+    cy.contains('div', 'Autocloseable success message. Hide in 5 seconds.')
+      .should('be.visible')
+    cy.wait(5000)
+    cy.contains('div', 'Autocloseable success message. Hide in 5 seconds.')
+      .should('not.exist')
+  });
+
+
+
+  it('Normal success message', () => {
+    cy.contains('button', 'Normal Success Message')
+      .click()
+    cy.contains('div', 'Normal success message')
+      .should('be.visible')
+    cy.get('.alert-success-manual > .close')
+      .click()
+    cy.contains('div', 'Normal success message')
+      .should('not.exist')
+  });
+
+})
---
+++ cypress/e2e/0 - StudyGroup/LambdaTestSite/alerts_js_lambda.cy.js
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+
+import { slowCypressDown } from 'cypress-slow-down'
+slowCypressDown()
+
+describe('Test for Study group on Lambda Test - Tabel', () => {
+
+  beforeEach(() => {
+    cy.setCookie('exit_popup_dismissed', 'closed');
+    cy.visit('https://www.lambdatest.com/selenium-playground/')
+    cy.contains('Javascript Alerts').click()
+  });
+
+  Cypress.on('uncaught:exception', () => false)
+
+
+  it('Js ALert', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'alert').as('alert')
+    })
+    cy.get('div[class="mt-30 rounded"]')
+      .eq(0)
+      .within(() => {
+        cy.get('button')
+          .contains('Click Me')
+          .click()
+        cy.get('@alert').should('have.been.calledOnce')
+      })
+  });
+
+  it('Confirm Box', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'confirm', () => true).as('conf')
+    })
+    cy.get('div[class="mt-30 rounded"]')
+      .eq(1)
+      .within(() => {
+        cy.contains('button', 'Click Me')
+          .click()
+        cy.get('@conf').should('have.been.calledOnce')
+        cy.get('#confirm-demo')
+          .should('be.visible')
+          .should('have.text', 'You pressed OK!')
+      })
+  });
+
+  it('Prompt Box', () => {
+    const text = "Lucian Hackerman"
+
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').as('prompt')
+        .returns(text)
+    })
+    cy.get('div[class="mt-30 rounded"]')
+      .eq(2)
+      .within(() => {
+        cy.contains('button', 'Click Me')
+          .click()
+        cy.get('@prompt')
+          .should('have.been.calledOnceWith', 'Please enter your name')
+        cy.get('#prompt-demo')
+          .should('be.visible')
+          .should('have.text', `You have entered '${text}' !`)
+      })
+  });
+})
+
